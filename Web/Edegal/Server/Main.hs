6c9e16d632d5cb4bfd7dbdb57101466838a93fce
Santtu Pajukanta <santtu@pajukanta.fi>
2014-05-15 21:08:17 +0300

    scotty: beam me up

--- Web/Edegal/Server/Main.hs
+++ Web/Edegal/Server/Main.hs
@@ -1,15 +1,26 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-import Data.Aeson (encode)
-import qualified Data.ByteString.Lazy.Char8 as BS
+import Control.Monad.Trans (liftIO)
 
-import qualified Web.Edegal.Models.Album as A
+import Data.Text (unpack)
+import Data.Aeson (encode)
+import Web.Scotty (scotty, get, regex, param, json)
+
+import qualified Web.Edegal.Models.Album as Album
 import Web.Edegal.Server.MetadataBackends.Base (getAlbum, putAlbum)
 import Web.Edegal.Server.MetadataBackends.TransactionalMemory (mkTransactionalMemoryMetadataBackend)
 
 
 main = do
   backend <- mkTransactionalMemoryMetadataBackend
-  putAlbum backend A.emptyRoot
-  maybeAlbum <- getAlbum backend "/"
-  BS.putStrLn $ encode maybeAlbum
+
+  -- XXX
+  putAlbum backend Album.emptyRoot
+
+  scotty 3000 $ do
+    get (regex "^/v2(/[a-zA-Z0-9/-]*)$") $ do
+      path <- param "1"
+      maybeAlbum <- liftIO $ getAlbum backend $ unpack path
+      json maybeAlbum
