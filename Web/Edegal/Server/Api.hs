5b933f3a6a9e008369942e0095e36de66e036b2e
Santtu Pajukanta <santtu@pajukanta.fi>
2014-05-15 22:36:32 +0300

    declare type for api

--- Web/Edegal/Server/Api.hs
+++ Web/Edegal/Server/Api.hs
@@ -5,11 +5,12 @@ module Web.Edegal.Server.Api (api) where
 import Control.Monad.Trans (liftIO)
 
 import Data.Text (unpack)
-import Web.Scotty (get, regex, param, json)
+import Web.Scotty (ScottyM, get, regex, param, json)
 
-import Web.Edegal.Server.MetadataBackends.Base (getAlbum)
+import Web.Edegal.Server.MetadataBackends.Base (MetadataBackend, getAlbum)
 
 
+api :: MetadataBackend a => a -> ScottyM ()
 api backend = do
   get (regex "^/api/v3(/[a-zA-Z0-9/-]*)$") $ do
     path <- param "1"
