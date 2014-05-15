1e030b3ab094bb658b813a069abcbaedef02809d
Santtu Pajukanta <santtu@pajukanta.fi>
2014-05-15 21:59:39 +0300

    more interestinger test data

--- Web/Edegal/Server/Main.hs
+++ Web/Edegal/Server/Main.hs
@@ -5,7 +5,6 @@ module Main where
 import Control.Monad.Trans (liftIO)
 
 import Data.Text (unpack)
-import Data.Aeson (encode)
 import Web.Scotty (scotty, get, regex, param, json)
 
 import qualified Web.Edegal.Models.Album as Album
@@ -13,11 +12,19 @@ import Web.Edegal.Server.MetadataBackends.Base (getAlbum, putAlbum)
 import Web.Edegal.Server.MetadataBackends.TransactionalMemory (mkTransactionalMemoryMetadataBackend)
 
 
+setupTestData backend = do
+  -- XXX
+  let (root, child) = Album.newChild Album.emptyRoot "foo"
+  putAlbum backend root
+  putAlbum backend child
+  return ()
+
+
 main = do
   backend <- mkTransactionalMemoryMetadataBackend
 
   -- XXX
-  putAlbum backend Album.emptyRoot
+  setupTestData backend
 
   scotty 3000 $ do
     get (regex "^/api/v3(/[a-zA-Z0-9/-]*)$") $ do
